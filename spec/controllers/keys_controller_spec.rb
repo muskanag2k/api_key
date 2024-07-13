0bd09cc21a00e00f664e4ad4b7612639dea1c265
Muskan Agarwal <muskan.agarwal@inito.com>
2024-07-13 23:30:25 +0530

    commit

--- spec/controllers/keys_controller_spec.rb
+++ spec/controllers/keys_controller_spec.rb
@@ -44,3 +44,41 @@ describe KeysController, type: :controller do
     end
   end
 end
+
+describe KeysController, type: :controller do
+  describe "PUT #unblock" do
+    let!(:blocked_key) { create(:key, status: 'blocked') }
+
+    it "unblocks a key" do
+      put :unblock, params: { key_value: blocked_key.value }
+
+      json_response = JSON.parse(response.body)
+      expect(json_response["key"]).to eq(blocked_key.value)
+      expect(blocked_key.reload.status).to eq('active')
+    end
+
+    it "returns not found for unavailable key" do
+      put :unblock, params: { key_value: 'unavailable_key' }
+
+      expect(response).to have_http_status(:not_found)
+    end
+  end
+end
+
+describe KeysController, type: :controller do
+  describe "DELETE #delete" do
+    let!(:key_to_delete) { create(:key) }
+
+    it "deletes a key" do
+      expect {
+        delete :delete, params: { key_value: key_to_delete.value }
+      }.to change(Key, :count).by(-1)
+    end
+
+    it "returns not found for unavailable key" do
+      delete :delete, params: { key_value: 'unavailable_key' }
+
+      expect(response).to have_http_status(:not_found)
+    end
+  end
+end
---
+++ spec/factories/keys.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :key do
+    value { '99' }
+    status { 'active' }
+  end
+end
